b07a5aae7ade569463220ffe0b5c206eed09dc98
vartult <vartult@rediffmail.com>
2019-02-27 00:43:34 +0530

    Layout AndroidX fix

--- app/src/main/java/com/cellfishpool/nonamewhatsapp/MainActivity.kt
+++ app/src/main/java/com/cellfishpool/nonamewhatsapp/MainActivity.kt
@@ -2,19 +2,26 @@ package com.cellfishpool.nonamewhatsapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.EditText
 import org.angmarch.views.NiceSpinner
 
 class MainActivity : AppCompatActivity() {
 
+    var phone: EditText?=null
+    var chat: EditText?=null
+    var share:Button?=null
+    var msg: Button?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var country= findViewById<NiceSpinner>(R.id.country) as NiceSpinner
-        val dataset= listOf("One","Two","Three")
-        country.attachDataSource(dataset)
-        var phone=findViewById<EditText>(R.id.phone)
+        phone=findViewById(R.id.phone)
+        msg=findViewById(R.id.msg)
+        chat=findViewById(R.id.chat)
+        share=findViewById(R.id.share)
+
+        
 
     }
 }
