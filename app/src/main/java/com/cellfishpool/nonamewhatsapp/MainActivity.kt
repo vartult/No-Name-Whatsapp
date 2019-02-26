f9808c3d9a7ec41be1220bb6d1e3d0b84240f7c7
vartult <vartult@rediffmail.com>
2019-02-27 02:03:21 +0530

    Phone Validation

--- app/src/main/java/com/cellfishpool/nonamewhatsapp/MainActivity.kt
+++ app/src/main/java/com/cellfishpool/nonamewhatsapp/MainActivity.kt
@@ -2,25 +2,44 @@ package com.cellfishpool.nonamewhatsapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
+import net.rimoto.intlphoneinput.IntlPhoneInput
 import org.angmarch.views.NiceSpinner
 
 class MainActivity : AppCompatActivity() {
 
-    var phone: EditText?=null
-    var chat: EditText?=null
-    var share:Button?=null
-    var msg: Button?=null
+    var phoneno: String="123"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        phone=findViewById(R.id.phone)
-        msg=findViewById(R.id.msg)
-        chat=findViewById(R.id.chat)
-        share=findViewById(R.id.share)
+        var phone=findViewById<IntlPhoneInput>(R.id.phone)
+        var msg=findViewById<EditText>(R.id.msg)
+        var chat=findViewById<Button>(R.id.chat)
+        var share=findViewById<Button>(R.id.share)
+        var phoneno: String?=null
+
+        chat.setOnClickListener{
+            validate(phone)
+        }
+        share.setOnClickListener{
+            validate(phone)
+        }
+
 
 
     }
+
+    private fun validate(phone: IntlPhoneInput?) {
+        if(phone.isValid)
+        {
+            phoneno= phone.phoneNumber.toString()
+        }
+    }
+
+
 }
