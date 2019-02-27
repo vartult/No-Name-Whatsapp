201149249a0e183d4b59b576a07cd6c122115a63
vartult <vartult@rediffmail.com>
2019-02-28 00:29:22 +0530

    Whatsapp Link

--- app/src/main/java/com/cellfishpool/nonamewhatsapp/MainActivity.kt
+++ app/src/main/java/com/cellfishpool/nonamewhatsapp/MainActivity.kt
@@ -1,45 +1,113 @@
 package com.cellfishpool.nonamewhatsapp
 
+import android.annotation.SuppressLint
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
-import net.rimoto.intlphoneinput.IntlPhoneInput
-import org.angmarch.views.NiceSpinner
+import com.lamudi.phonefield.PhoneEditText
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.content.ComponentName
+import android.content.Context
+import java.net.URISyntaxException
+import android.content.Context.INPUT_METHOD_SERVICE
+import android.support.v4.content.ContextCompat.getSystemService
+import android.text.ClipboardManager
+import android.view.inputmethod.InputMethodManager
+import android.content.ClipData
+
+
+
 
 class MainActivity : AppCompatActivity() {
 
     var phoneno: String="123"
+    var valid=true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var phone=findViewById<IntlPhoneInput>(R.id.phone)
+        var phone=findViewById<PhoneEditText>(R.id.phone)
         var msg=findViewById<EditText>(R.id.msg)
         var chat=findViewById<Button>(R.id.chat)
         var share=findViewById<Button>(R.id.share)
-        var phoneno: String?=null
 
+        phone.setDefaultCountry("IN")
         chat.setOnClickListener{
             validate(phone)
+            preprocess()
+            openInWhatsapp()
+
         }
         share.setOnClickListener{
+            var extradata:String?=msg.text.toString()
             validate(phone)
-        }
-
-
-
-    }
-
-    private fun validate(phone: IntlPhoneInput?) {
-        if(phone.isValid)
-        {
-            phoneno= phone.phoneNumber.toString()
+            preprocess()
+            shareaddress(extradata)
         }
     }
 
+    private fun shareaddress(extradata: String?) {
+        var intent= Intent(Intent.ACTION_SEND)
+        intent.setType("text/plain")
+        var lineshare= "You can chat via \n" + "https://wa.me/" + phoneno +"\n MSG FOR YOU \n"+extradata
+        intent.putExtra(Intent.EXTRA_TEXT,lineshare)
+        startActivity(Intent.createChooser(intent,"Share Link Using"))
+
+    }
+
+    private fun preprocess() {
+
+        phoneno=phoneno.replace("+", "");
+        phoneno=phoneno.replace("^0", "");
+
+    }
+
+
+    private fun validate(phone: PhoneEditText) {
+
+            if (phone.isValid) {
+                phone.setError(null)
+                hideKeyboard(phone)
+                valid=false
+            } else {
+                phone.setError("Invalid Phone Number")
+                hideKeyboard(phone)
+            }
+
+
+        phoneno = phone.getPhoneNumber()
+    }
+
+
+
+    private fun openInWhatsapp() {
+        try {
+
+            Toast.makeText(applicationContext,"Processing...",Toast.LENGTH_SHORT).show()
+            val intent = Intent("android.intent.action.MAIN")
+            intent.component = ComponentName("com.whatsapp", "com.whatsapp.Conversation")
+            intent.putExtra("jid", phoneno + "@s.whatsapp.net")
+            intent.putExtra("displayname", "+" + phoneno)
+            startActivity(Intent.parseUri("https://wa.me/" + phoneno, 0))
+            //startActivity(Intent.parseUri("whatsapp://send/?" + phoneno, 0))
+        } catch (ignore: URISyntaxException) {
+            ignore.printStackTrace()
+        } catch (e: ActivityNotFoundException) {
+            Toast.makeText(applicationContext, "Whatsapp not installed", Toast.LENGTH_LONG).show()
+        }
+
+    }
+
+    @SuppressLint("ServiceCast")
+    protected fun hideKeyboard(v: View) {
+        val imm = v.context.applicationContext.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(v.windowToken, 0)
+    }
+    
 
 }
